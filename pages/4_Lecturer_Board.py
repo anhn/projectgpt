a1c77dff4a7bbea10f8ac6d252fc3822cfbfacbd
anhn <anhnd85@gmail.com>
2024-11-26 19:45:44 +0100

    Update 4_Lecturer_Board.py

--- pages/4_Lecturer_Board.py
+++ pages/4_Lecturer_Board.py
@@ -4,14 +4,11 @@ import pandas as pd
 feedback1 = "Students are divided into teams of six to eight students. Teams are formed by randomly assigning students; the members generally do not know each other beforehand. This is a typical situation in a real life setting, especially when working as a consultant. The students are free to choose the methods that best fit their projects. In some cases, the development approaches are enforced by customers due to existing working processes they have adopted in their organizations. Each team decides by themselves about their management and leadership mechanisms. In most of the cases, students organize themselves using a flat organizational structure within the team. Decision making is often done collectively and the reasons for important decisions are documented in the final reports. There are several lectures at the beginning of the course teaching topics such as project management, team dynamics, Scrum, architecture, and testing."
 feedback2 = "Students typical engage in three phases of their projects, which are planning, execution and closing (refer to Figure 1). In the planning phase, the students get to know their team members, customers, and their project requirements. The students need to decide roles and areas of responsibility for each member. They also make a preliminary project plan and set up the working environment. In the execution phase, the student teams typically carry out sprints with frequent deliveries to their customers. Each project covers fundamental SE activities, i.e., requirement elicitation, architecture level design, coding, and testing. Students also write a project report that reflects their team’s progress. In the closing phase, the project results are submitted in a report; they are also demonstrated and presented to the customer and course staff (e.g., course instructor, supervising teaching assistant)."
 feedback3 = "The course deliverables for each team include (1) an end of term demonstration of the software, (2) a presentation, and (3) a final project report. The course project work is evaluated on the basis of the quality of the project reports, the functioning prototype of the system, the presentation delivered at the end of the course, and the team dynamics. Customers consider their experience working with the team, the value of the software developed, and the delivered report in their evaluations. Supervisors, who are typically teaching assistants in the course, evaluate their teams’ performance and learning based on their observations throughout the course. The faculty member responsible for the course involves both the customer and the supervisor in an evaluation meeting to determine the final marks for students. In the evaluation scheme, a team with a poorly functioning application does not necessarily receive a low grade. Student teams that have difficult projects with poor functionality, but express quick learning curves and effective teamwork can achieve a good grade."
-exercise1 = "Modul 2 – exercise 3.
-
+exercise1 = 'Modul 2 – exercise 3. 
 Forteller denne listen behov fra alle relevante interessenter?
-Ønskelisten fra prosjekteier inkluderer behovene til alle relevante interessenter. Relevante interessenter inkluderer prosjekteier, nåværende og potensielle kunder. Punktene på ønskelisten favner bredt og tar hensyn til ulike organisasjoner, enkeltpersoner og grupper.
-
-Er disse "ønskene" klare nok til å lede ledelsen og utviklingen av prosjektet?
+Ønskelisten fra prosjekteier inkluderer behovene til alle relevante interessenter. Relevante interessenter inkluderer prosjekteier, nåværende og potensielle kunder. Punktene på ønskelisten favner bredt og tar hensyn til ulike organisasjoner, enkeltpersoner og gripper.
+Er disse ønskene klare nok til å lede ledelsen og utviklingen av prosjektet?
 Innholdet i ønskelisten gir en god beskrivelse av elementer, innhold og design, noe som gir utviklerne et godt utgangspunkt til å lede utviklingen av prosjektet.  Før utviklingen av prosjektet kan starte vil det imidlertid være viktig å utdype noen detaljer rundt ønskene, som hvilke fasiliteter som skal med i de forskjellige rommene. Ved å ha god kommunikasjon før utviklingen av prosjektet starter, unngår vi misforståelser. 
-
 Suksesskriteriene for prosjektet kan være:
 ·       En responsiv og brukervennlig nettside, både på mobile enheter og på PC, med tydelig informasjon om produkter og tjenester.
 ·       Økt synlighet og tiltrekke seg flere kunder.
@@ -21,10 +18,9 @@ Suksesskriteriene for prosjektet kan være:
 ·       Analyse av de besøkendes adferd på nettsiden: hvor lang tid bruker de besøkende inne på nettsiden, hvor lang tid tar det å gjøre en booking og hvilke elementer av nettsiden engasjerer mest.
 ·       Merkevarebygging og økt legitimitet via positiv omtale og anmeldelser fra nåværende medlemmer.
 ·       Ferdigstillelse av produkt innenfor den angitte tiden.
-
 Hvor lang tid tar det for et team på tre-fem personer inkludert deg?
 Tidsrammen for teamet på fire personer, inkludert prosjektleder, beregnes å vare fra januar til april 2024. Innenfor disse fire månedene vil prosjektgruppen ha vært igjennom planlegging, design, utvikling og lansering av nettsiden. Dette vil være en realistisk tidsramme, basert på tidligere erfaringer med lignende prosjekter og størrelsen på prosjektet. 
-"
+'
 exercise2="Modul 2 – exercise 4.
 
 Hovedsakelig kan man si at de aller fleste punktene på ønskelistene overholder disse seks punktene. Likevel finner man noen unntak, her er noen av dem:

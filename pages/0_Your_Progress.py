d0a85c3afa8c9467cf365e8516a85a6f67352f72
anhn <anhnd85@gmail.com>
2024-11-27 23:13:33 +0100

    Update 0_Your_Progress.py

--- pages/0_Your_Progress.py
+++ pages/0_Your_Progress.py
@@ -10,13 +10,14 @@ st.header("Summary of your team progress")
 today = date.today()
 st.write("Here we summarize your learning progress up to ", today)
 
+my_coursetime = st.progress(20, text="You have completed 3/ 15 study weeks of this course")
 my_assignment = st.progress(20, text="You have completed 5/ 25 exercises in this course")
 my_milestones = st.progress(33, text="You have completed 1/ 3 obligatory milestones in this course")
 my_meeting = st.progress(33, text="You have completed 1/ 10 obligatory meetings in this course")
 my_agile = st.progress(33, text="You have completed 5/ 10 suggested Agile practices in this course")
 my_product = st.progress(10, text="You have completed 2/ 20 requirements in this course")
 
-st.write("Here we summarize your learning progress up to ", today)
+st.write("Here is our feedback on your progress")
 
 st.write("Team: It looks like you have established your team! Do you get more meeting with other team members? Have you considered using a team contract to improve commitment within the team?")
 st.write("Customer: Here is the summary of your conversation with the customer so far. Make sure the request from the customers correctly understood!")

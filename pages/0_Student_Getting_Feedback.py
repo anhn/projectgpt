5eb4de37f6285d937ee0a4c6f3f4af11245d2a51
anhn <anhnd85@gmail.com>
2024-12-16 11:08:57 +0100

    Update 0_Student_Getting_Feedback.py

--- pages/0_Student_Getting_Feedback.py
+++ pages/0_Student_Getting_Feedback.py
@@ -50,9 +50,9 @@ with tab1:
         st.session_state["openai_model"] = "gpt-4o"        
     if "messages" not in st.session_state:
         st.session_state.messages = []        
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])        
+    #for message in st.session_state.messages:
+    #    with st.chat_message(message["role"]):
+    #        st.markdown(message["content"])        
     if prompt := st.chat_input("What is up?"):
         st.session_state.messages.append({"role": "system", "content": get_course_description("description")})
         st.session_state.messages.append({"role": "system", "content": get_course_description("report_guideline")})
